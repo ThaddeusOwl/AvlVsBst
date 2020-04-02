3984b0e11efb5e1395509ce6ccc56f4255c723a4
Thaddeus Owl <61245401+ThaddeusOwl@users.noreply.github.com>
2020-04-02 21:59:22 +0200

    Summary Stats runs :) but needs debug

--- src/SummaryStats.java
+++ src/SummaryStats.java
@@ -4,21 +4,17 @@ import java.util.Scanner;
 
 public class SummaryStats{
 
-   String fileName = "DataSet10";
-   String rowsString = "10";
-   int rows = Integer.parseInt(rowsString); //temp variable
-      
-   
-   public String[][] toRawArray(String fileName, int rows){
+
+   public String[][] toRawArray(String fileName, int rows) throws FileNotFoundException{
       String[][] toReturn = new String[5][rows];
-      Scanner file = new Scanner(new File(fileName));
+      Scanner file = new Scanner(new File(fileName+".csv"));
       file.nextLine();
       file.nextLine();
       for(int j=0; j<rows; j++){ //each row
          String line = file.nextLine();
-         String[] lineSplit = line.split(",")
+         String[] lineSplit = line.split(",");
          for (int i=0; i<5; i++){
-            rawData[i][j] = lineSplit[i]; //fill columns, hold row constant
+            toReturn[i][j] = lineSplit[i]; //fill columns, hold row constant
          }   
       } 
       return toReturn;   
@@ -30,27 +26,29 @@ public class SummaryStats{
          int min=10000;
          int max=0;
          int avg=0;
-         for(int j=0; j<rawData[i][].length; j++){
+         for(int j=0; j<rawData[i].length; j++){
             min = Math.min(min, Integer.parseInt(rawData[i][j]));
             max = Math.max(max, Integer.parseInt(rawData[i][j]));
             avg = avg + Integer.parseInt(rawData[i][j]);
          }
-         avg = avg/rawData[i][].size;
-         toReturn[i][]={String.valueOf(min), String.valueOf(max), String.valueOf(avg)};
+         avg = avg/rawData[i].length;
+         toReturn[i][0] = String.valueOf(min);
+         toReturn[i][1] = String.valueOf(max);
+         toReturn[i][2] = String.valueOf(avg);
       }
       return toReturn; 
    }
    
-   public static void main(String[] args){
+   public void runMethod() throws FileNotFoundException{
       int[] dataset=new int[]{10, 297, 593, 947, 1243, 1647, 1943, 2227, 2543, 2976};
       for(int n=0; n<dataset.length; n++){
-         String fileName = ("DataSet"+dataset[n])
-         String[][] summaryData = SummaryData.toSummaryArray(toRawArray(fileName, dataset[n]));
+         String fileName = ("DataSet"+dataset[n]);
+         String[][] summaryData = toSummaryArray(toRawArray(fileName, dataset[n]));
          for(int j=0; j<n; j++){
             for(int i=0; i<5; i++){
                System.out.print(summaryData[i][j]+",");
             }
-            System.out.print("\n")
+            System.out.print("\n");
          }
       }
    }
---
+++ src/runSummaryStats.java
@@ -0,0 +1,12 @@
+import java.io.FileNotFoundException;
+
+public class runSummaryStats{
+
+public static void main(String[] args) throws FileNotFoundException{
+
+  SummaryStats s = new SummaryStats();
+  s.runMethod();
+
+}
+
+}
